24df3a8374d77949b4ae5b70e0f433c72ffcc28f
danielaguardabascio1 <guardabascio.daniela@gmail.com>
2018-10-24 00:55:04 +0200

    Commit Exsercise number six and partial number seven

---
+++ Homework_2/Es_6/javascript.js
@@ -0,0 +1,30 @@
+var colori = Array ('#dd2493','#236e96','#5deb2e','#ff1111');
+var myVar;
+var go=true;
+var n=0;
+
+function onLoad() {    
+    if(go==true)
+            myVar = window.setInterval(changeBackground, 1500);
+}
+
+
+function changeBackground() {
+    n++;
+    document.body.style.background = colori[n];
+}
+
+
+
+function stop(){
+   go=false; 
+}
+
+function riavvia(){
+    go=true;
+     myVar = window.setInterval(changeBackground, 1500);
+    console.log(go);
+}
+
+
+
---
+++ Homework_2/Es_7/javascript.js
@@ -0,0 +1,44 @@
+function limitText(limitField, limitNum) {
+    if (limitField.value.length > limitNum) {
+        limitField.value = limitField.value.substring(0, limitNum);
+        window.alert("hai inserito piu di 5 cifre");
+    }
+
+}
+
+capId.limitText();
+
+function validate() {
+
+    var element = document.querySelector("#nomcogn").value;
+    var regex = /\d/g;
+    if (regex.test(element))
+        window.alert("Il campo non puo contenere numeri");
+
+}
+
+
+function invioDati() {
+    var nomCogn = document.querySelector("#nomcogn").value;
+    var sex = document.querySelector("#sex").value;
+    var ateneo = document.querySelector("#at").value;
+    var capid = document.querySelector("#capId").value;
+    var lavoratore = document.querySelector("#lavoroId").value;
+    var descrizione = document.querySelector("#descLav").value;
+    
+    console.log(nomCogn);
+    
+    if (nomCogn=='')
+        window.alert("Non è stato compuilato il campo nome cognome");
+    
+    if(sex=='')
+        window.alert("Non è stato selezionato il sesso");
+    
+    if(ateneo=='')
+        window.alert("Non è stato selezionato un ateneo");
+    
+    if(lavoratore!='')
+        if(descrizione=='')
+                window.alert("Non è stato compilato ilcampo per la descrizioen del lavoro");
+
+}
