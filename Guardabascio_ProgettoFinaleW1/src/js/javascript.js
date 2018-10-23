63419c12316c518fce7e73ad4dc9e4cb48e52a15
danielaguardabascio1 <guardabascio.daniela@gmail.com>
2018-10-23 14:38:10 +0200

    Progetto Finale Css e Html

---
+++ Guardabascio_ProgettoFinaleW1/src/js/javascript.js
@@ -0,0 +1,10 @@
+<script>
+
+    document.getElementById('change').onclick = changeColor;   
+
+    function changeColor() {
+        document.body.style.color = "purple";
+        return false;
+    }   
+
+</script>
