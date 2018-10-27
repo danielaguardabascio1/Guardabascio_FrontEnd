2f32bd9ec4a942437bc853ca32ae430d3ab7d88b
danielaguardabascio1 <guardabascio.daniela@gmail.com>
2018-10-27 11:28:00 +0200

    Restyling img GitHub

--- Guardabascio_ProgettoFinaleW1/src/js/bundle.js
+++ Guardabascio_ProgettoFinaleW1/src/js/bundle.js
@@ -10396,37 +10396,4 @@ $(document).ready(
         });
     });
 
-
-
-/*
-// Funzione per nascondere i coockie
-CookieId.addEventListener('click', function () {
-    document.getElementById('cookie').style.display = "none";
-    document.getElementById('cookie').classList.adfunction doAjax(){
-    $.ajax({
-    url: 'ajax_file.json',
-    method: 'POST',
-    data: {key: 'value'},
-    success: function(result){
-    alert(result);
-    }
-    
-    });
-    }
-var listArticle= document.getElementsByClassName('like');
-
-for(var i = 0; i<listArticle.length ; i++){
-    listArticle[i].addEventListener('click' , function (){
-        if(this.classList.contains('like')){
-             this.classList.toggle('like_green');
-        }
-        else { 
-        this.classList.toggle('like');
-    }
-            
-    })
-}
-
-*/
-
 },{"jquery":1}]},{},[2]);
