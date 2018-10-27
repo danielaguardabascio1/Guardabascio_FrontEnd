cfbf3fbfe06009048ed38699bb12fc98a4f0a2af
danielaguardabascio1 <guardabascio.daniela@gmail.com>
2018-10-27 20:05:59 +0200

    refactoring code , add rendering using mustache

--- Guardabascio_ProgettoFinaleW1/src/js/javascript.js
+++ Guardabascio_ProgettoFinaleW1/src/js/javascript.js
@@ -1,34 +1,44 @@
 const $ = require('jquery');
+const Mustache = require('mustache');
 
-$(document).ready(
-    () => {
+
+$(document).ajaxComplete(function() {
+    $('.like').on('click', event => {
+            $(event.currentTarget).toggleClass('like_green')
+        });
+            
         $('#CookieId').on('click', () => {
             $('#cookie').hide();
         });
 
-        $('.like').on('click', event => {
-            $(event.currentTarget).toggleClass('like_green')
-        });
+});
 
 
-/*
+$(document).ready(
+    () => {
         $.ajax({
-            url: 'ajax_file.json',
+            url: 'articles.json',
             method: 'GET',
             success: function (result) {
-                var output='';
-                console.log('ciaoo');
-                //for (var i = 0; i < result.articolo.length; i++) {
-                $.each(result.articolo , function(key , value)){
-                console.log('ciaooo for'); 
-                    // output += "<p>" + result[i].articlo + "</p>";
-                }
-                //$('#Remote').html(output);
+                var output = '';
+                $.each(result.articles, function (key, value) {
+                    var template =
+                        "<article class='style_article col-xs-12 col-sm-6 col-md-4 col-lg-4'><h1 class='title_article'>{{title}}</h1> <img src='{{image}}' width='100%' height='auto' class='img-fluid' alt='Responsive image'> <span class='tech btn btn-xs' role='button'>{{tech}}</span><p> {{text}}</p><script type='button' class='like btn btn-primary' role='button'>{{like}}</script></article>";
+
+                    var html = Mustache.to_html(template, result.articles[key]);
+                    output += html;
+
+                });
+
+                $('#articoli').html(output);
             },
             error: function (result) {
                 alert('Errore caricamento');
             }
 
         });
-        */
+
     });
+
+
+
